25a86a5937a08b43aefbaf24b08f66b074c6aa99
Trevor Stavropoulos <Trevor.Stavropoulos@gmail.com>
2016-06-22 21:10:10 -0700

    Removing QtConcurrent from the Build script

--- GenerateBuildFiles.cmd
+++ GenerateBuildFiles.cmd
@@ -29,9 +29,9 @@ popd
 pushd %SD_TREE%
 
 @echo Getting Qt libraries
-@ROBOCOPY %QTDIR%\bin %SD_TREE%\output\bin\release Qt5Core.dll Qt5Gui.dll Qt5Widgets.dll Qt5WinExtras.dll Qt5Concurrent.dll /NFL /NDL /NJH /NJS 1> nul
+@ROBOCOPY %QTDIR%\bin %SD_TREE%\output\bin\release Qt5Core.dll Qt5Gui.dll Qt5Widgets.dll Qt5WinExtras.dll /NFL /NDL /NJH /NJS 1> nul
 
-@ROBOCOPY %QTDIR%\bin %SD_TREE%\output\bin\debug Qt5Cored.dll Qt5Guid.dll Qt5Widgetsd.dll Qt5WinExtrasd.dll Qt5Concurrentd.dll Qt5Concurrentd.pdb Qt5Cored.pdb Qt5Guid.pdb Qt5Widgetsd.pdb Qt5WinExtrasd.pdb /NFL /NDL /NJH /NJS 1> nul
+@ROBOCOPY %QTDIR%\bin %SD_TREE%\output\bin\debug Qt5Cored.dll Qt5Guid.dll Qt5Widgetsd.dll Qt5WinExtrasd.dll Qt5Cored.pdb Qt5Guid.pdb Qt5Widgetsd.pdb Qt5WinExtrasd.pdb /NFL /NDL /NJH /NJS 1> nul
 
 @ROBOCOPY %QTDIR%\plugins\platforms %SD_TREE%\output\bin\release\platforms qwindows.dll /NFL /NDL /NJH /NJS 1> nul
 
